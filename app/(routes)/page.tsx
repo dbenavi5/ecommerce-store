5bb12c22ea5811ec034411ef970120cf347a126a
dbenavi5 <56004267+dbenavi5@users.noreply.github.com>
2023-07-15 20:40:00 -0700

    environment setup & featured products

---
+++ app/(routes)/page.tsx
@@ -0,0 +1,16 @@
+import Container from "@/components/ui/container";
+
+const HomePage = async () => {
+  return (
+    <Container>
+      <div className="space-y-10 pb-10">
+        {/* Billboard */}
+        <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+          {/* ProductList */}
+        </div>
+      </div>
+    </Container>
+  );
+};
+
+export default HomePage;
---
+++ components/footer.tsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+  return (
+    <footer className="bg-white border-t">
+      <div className="mx-auto py-10">
+        <p className="text-center text-sm text-black">
+          &copy; 2023 FakeStore Inc., All Rights Reserved
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
